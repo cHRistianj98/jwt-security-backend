ccbbb414176f22254e899e26a43d22b6082da6d6
vanvi <kania.praca@wp.pl>
2023-06-17 19:45:05 +0200

    Create JWT service

--- src/main/java/com/chrisianj98/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/chrisianj98/security/config/JwtAuthenticationFilter.java
@@ -10,16 +10,27 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NonNull final HttpServletRequest request,
             @NonNull final HttpServletResponse response,
             @NonNull final FilterChain filterChain
     ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        if (Objects.isNull(authHeader) || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        int positionOfFirstJwtCharacterInHeader = 7;
+        final String jwt = authHeader.substring(positionOfFirstJwtCharacterInHeader);
+        final String userEmail = jwtService.extractUsername(jwt);
 
     }
 }
---
+++ src/main/java/com/chrisianj98/security/config/JwtService.java
@@ -0,0 +1,10 @@
+package com.chrisianj98.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(final String token) {
+        return null;
+    }
+}
